3d85a61b88c32e5923330b477b6ab818ab3c1c2b
Sinjo Alex <sinjoalex@yahoo.com>
2024-01-07 19:09:32 +0530

    Completed and tested this file app.py

--- app.py
+++ app.py
@@ -4,14 +4,24 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Define what the app does
+  # """
+    # TODO:
+    # 1. Capture first name & last name
+    # 2. If either is not provided: respond with an error
+    # 3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
+    # 4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
+    # 5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
+    # """
 @app.get("/greet")
 def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
-    return jsonify("TODO")
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+    if not fname and not lname:
+        return jsonify({"status": "error", "message": "Both first name and last name are required"}), 400
+    elif fname and not lname:
+        response = jsonify({"data": f"Hello, {fname}!"})
+    elif not fname and lname:
+        response = jsonify({"data": f"Hello, Mr.{lname}!"})
+    else:
+        response = jsonify({"data": f"Is you name {fname} {lname}?"})
+    return response
